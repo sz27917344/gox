e71ece7f3b1dcff790d7d6084858defdd90af37d
shz <shuzhan@onein.com>
2022-03-06 16:47:17 +0800

    feat: 初次提交

---
+++ stream/sort.go
@@ -0,0 +1,13 @@
+package stream
+
+// 排序相关类
+type sortByFun[T any] struct {
+	data []T
+	fun  func(s1 T, s2 T) bool
+}
+
+func (s sortByFun[T]) Len() int       { return len(s.data) }
+func (s *sortByFun[T]) Swap(i, j int) { s.data[i], s.data[j] = s.data[j], s.data[i] }
+func (s *sortByFun[T]) Less(i, j int) bool {
+	return s.fun(s.data[i], s.data[j])
+}
